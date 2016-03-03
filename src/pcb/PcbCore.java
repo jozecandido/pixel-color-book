4bc28b0ab7db9d9262732288811b96a62f4e8e96
joao <jggprudencio@gmail.com>
2016-03-03 17:24:44 -0300

    Definição dos tamanhos das grids P, M e G. Método de cálculo da distância de dias cores.

--- src/pcb/PcbCore.java
+++ src/pcb/PcbCore.java
@@ -1,5 +1,6 @@
 package pcb;
 
+import java.awt.Color;
 import java.util.List;
 
 public class PcbCore {
@@ -19,15 +20,28 @@ public class PcbCore {
 		//PRE-PROCESSAMENTO
 		//TODO: processar stream?
 		//TODO: Mudar contraste e saturação
-		//TODO: redimensionar para tamanhos (P,M e G:50px,72px)
+		//TODO: redimensionar para tamanhos (Fácil: 21x31, Médio: 36x53 e Avançado: 50x72)
 		
 		//PROCESSAMENTO
 		//TODO: ?? Traduzir mapa de cores para byte|rgb|o que for ??
 		//TODO: Mapear pixels da imagem
-		//TODO: Mapear pixel para lápis sqrt( (R1-R2)ˆ2 + (G1-G2)ˆ2 + (B1-B2)ˆ2)  
+		//TODO: Mapear pixel para lápis sqrt( (R1-R2)ˆ2 + (G1-G2)ˆ2 + (B1-B2)ˆ2) (implementado abaixo) 
 		//TODO: Gerar tabela mapeada
 		
 		//POS-PROCESSAMENTO | OUTPUT
 		//TODO: Gerar PDF		
+	}
+	
+	public static double distanceTo(Color pixelColor, Color pencilColor) {
+		double redDistance = pixelColor.getRed() - pencilColor.getRed();
+		double greenDistance = pixelColor.getGreen() - pencilColor.getGreen();
+		double blueDistance = pixelColor.getBlue() - pencilColor.getBlue();
+
+		double distanceTo = (redDistance * redDistance) 
+				+ (greenDistance * greenDistance)
+				+ (blueDistance * blueDistance);
+		distanceTo = Math.sqrt(distanceTo);
+
+		return distanceTo;
 	}
 }
