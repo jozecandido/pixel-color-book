3084046048adf7eb8271b3ce81b748ad77781994
Jose Candido <candidofilho@gmail.com>
2016-03-02 21:09:07 -0300

    TODOS!!!!

--- src/PcbCore.java
+++ src/PcbCore.java
@@ -3,7 +3,29 @@ public class PcbCore {
 
 	public static void main(String[] args) {
 		System.out.println("inicio");
+		//CONFIGURACAO
+		//TODO: Definir mapa de cores
+		//TODO: Definir conjunto de lapis
 
+						
+		//INPUT
+		//TODO: receber arquivo imagem
+		//TODO: receber TAMANHO
+		//TODO: receber conjunto de lápis 
+		
+		//PRE-PROCESSAMENTO
+		//TODO: processar stream?
+		//TODO: Mudar contraste e saturação
+		//TODO: redimensionar para tamanhos (P,M e G:50px,72px)
+		
+		//PROCESSAMENTO
+		//TODO: ?? Traduzir mapa de cores para byte|rgb|o que for ??
+		//TODO: Mapear pixels da imagem
+		//TODO: Mapear pixel para lápis sqrt( (R1-R2)ˆ2 + (G1-G2)ˆ2 + (B1-B2)ˆ2)  
+		//TODO: Gerar tabela mapeada
+		
+		//POS-PROCESSAMENTO | OUTPUT
+		//TODO: Gerar PDF		
 	}
 
 }
