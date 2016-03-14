1e26b9cec0319bf94186eddd06b2f0458c26a77a
Jose Candido <candidofilho@gmail.com>
2016-03-14 19:28:17 -0300

    Atualizando ImageProcessor

--- src/pcb/imaging/processor/ImageProcessor.java
+++ src/pcb/imaging/processor/ImageProcessor.java
@@ -3,9 +3,12 @@ package pcb.imaging.processor;
 import java.io.IOException;
 
 import pcb.imaging.ColorImageMapping;
-import pcb.model.ColorPencilBox;
 import pcb.model.ImageSize;
 
 public interface ImageProcessor {
-	public ColorImageMapping processImage(byte[] image, ColorPencilBox pencilBox, ImageSize paintSize) throws IOException;
+	public ColorImageMapping processImage(byte[] image, ImageSize paintSize) throws IOException;
+	
+	public static ImageProcessor getCurrentProcessor() {
+		return System.getProperty("java.version") != "0" ? new JDKProcessor() : new AndroidProcessor();
+	} 
 }
