b45268905ecd927e5a071e80f4fb575b666156fb
Jose Candido <candidofilho@gmail.com>
2016-03-14 22:46:02 -0300

    adicionando projeto web

---
+++ pcb-web/src/main/java/pcb/web/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pcb.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PcbWebApplication.class);
+	}
+
+}
---
+++ pcb-web/src/test/java/pcb/web/PcbWebApplicationTests.java
@@ -0,0 +1,18 @@
+package pcb.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PcbWebApplication.class)
+@WebAppConfiguration
+public class PcbWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
